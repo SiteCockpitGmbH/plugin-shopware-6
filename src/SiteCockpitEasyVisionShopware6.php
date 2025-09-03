b443b5c0ee8dc42e89d66f11dbf828dd3b054465
Jahir <jahir.hossain@solution360.de>
2025-09-03 11:19:48 +0200

    v3.0.0

--- src/SiteCockpitEasyVisionShopware6.php
+++ src/SiteCockpitEasyVisionShopware6.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace S360SiteCockpitEasyVision;
+namespace SiteCockpitEasyVisionShopware6;
 
 use Doctrine\DBAL\Connection;
 use Shopware\Core\Framework\Plugin;
@@ -10,7 +10,7 @@ use Shopware\Core\Framework\Plugin\Context\InstallContext;
 use Shopware\Core\Framework\Plugin\Context\UninstallContext;
 use Shopware\Core\Framework\Plugin\Context\UpdateContext;
 
-class S360SiteCockpitEasyVision extends Plugin
+class SiteCockpitEasyVisionShopware6 extends Plugin
 {
     public function install(InstallContext $installContext): void
     {
