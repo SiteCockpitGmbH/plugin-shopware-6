fe31585c78f1219aa7a940ebd976fa79f8af0d3d
Sebastian Meyer <sebastian.meyer@solution360.de>
2025-11-10 10:40:58 +0100

    Heavily refactored and simplified code

--- src/SiteCockpitEasyVisionShopware6.php
+++ src/SiteCockpitEasyVisionShopware6.php
@@ -3,12 +3,14 @@
 namespace SiteCockpitEasyVisionShopware6;
 
 use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Driver\Exception;
 use Shopware\Core\Framework\Plugin;
 use Shopware\Core\Framework\Plugin\Context\ActivateContext;
 use Shopware\Core\Framework\Plugin\Context\DeactivateContext;
 use Shopware\Core\Framework\Plugin\Context\InstallContext;
 use Shopware\Core\Framework\Plugin\Context\UninstallContext;
 use Shopware\Core\Framework\Plugin\Context\UpdateContext;
+use Shopware\Core\System\SystemConfig\SystemConfigService;
 
 class SiteCockpitEasyVisionShopware6 extends Plugin
 {
@@ -25,9 +27,11 @@ class SiteCockpitEasyVisionShopware6 extends Plugin
             return;
         }
 
-        $connection = $this->container->get(Connection::class);
-
-        $connection->executeStatement('DROP TABLE IF EXISTS `domain_sitekey`');
+        $currentVersion = $uninstallContext->getCurrentPluginVersion();
+        if(version_compare($currentVersion, '2.1.0', '<')) {
+            $connection = $this->container->get(Connection::class);
+            $connection?->executeStatement('DROP TABLE IF EXISTS `domain_sitekey`');
+        }
     }
 
     public function activate(ActivateContext $activateContext): void
@@ -44,7 +48,29 @@ class SiteCockpitEasyVisionShopware6 extends Plugin
 
     public function update(UpdateContext $updateContext): void
     {
-        // Update necessary stuff, mostly non-database related
+        $currentVersion = $updateContext->getCurrentPluginVersion();
+        if(version_compare($currentVersion, '2.1.0', '<')) {
+            /** @var SystemConfigService|null $systemConfigService */
+            $systemConfigService = $this->container->get(SystemConfigService::class);
+            /** @var Connection|null $connection */
+            $connection = $this->container->get(Connection::class);
+            if($systemConfigService === null || $connection === null) {
+                return;
+            }
+            try {
+                $result = $connection->executeQuery('SELECT * FROM `domain_sitekey`')->fetchAllAssociative();
+                // Map configuration into system configuration, but set it just once for sales channel null
+                if(!empty($result)) {
+                    $domainKey = $result[0]['sitekey'] ?? null;
+                    if(!empty($domainKey)) {
+                        $systemConfigService->set('SiteCockpitEasyVisionShopware6.config.easyvisionIntegrationKey', $domainKey, null);
+                    }
+                }
+                $connection->executeStatement('DROP TABLE IF EXISTS `domain_sitekey`');
+            } catch (Exception|\Doctrine\DBAL\Exception $e) {
+                // Not much to do here - we can ignore this.
+            }
+        }
     }
 
     public function postInstall(InstallContext $installContext): void
